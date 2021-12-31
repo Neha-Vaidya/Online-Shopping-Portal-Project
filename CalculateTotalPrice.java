a067e465967d2ba01e80f13d021d45ed95ea1936
Neha-Vaidya <91827825+Neha-Vaidya@users.noreply.github.com>
2021-12-31 10:05:46 +0530

    Add files via upload

---
+++ CalculateTotalPrice.java
@@ -0,0 +1,15 @@
+
+package i2.logic;
+
+public class CalculateTotalPrice {
+	//public static double total=0;
+    public static void add(double price , double total){
+
+        total = total + price;
+    }
+    
+
+   
+
+}
+
